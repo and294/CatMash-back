0edf7db0e2de13b33ff43591c978fb992e97861d
Andrea <and29ferraro@gmail.com>
2023-03-29 09:42:50 +0200

    first commit

---
+++ models/cats.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const catSchema = mongoose.Schema(
+  {
+url: String,
+id: String,
+vote: Number
+  }
+);
+
+const Cat = mongoose.model("cats", catSchema);
+
+module.exports = Cat;
---
+++ routes/cats.js
@@ -0,0 +1,34 @@
+var express = require('express');
+const Cat = require('../models/cats');
+var router = express.Router();
+
+//Récupérer la liste de tous les chats
+router.get('/', (req, res, next) => {
+  Cat.find().then((data) => {
+    res.json({ cats: data });
+  });
+});
+
+//Route pour voter pour un chat
+router.get('/updateVote', (req, res) => {
+  Cat.findOneAndUpdate(
+    { id: req.body.id },
+    { $inc: { vote: 1 } },
+    { new: true }
+  )
+    .then(data => {
+      res.json({ cats: data });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+
+
+
+
+
+
+module.exports = router;
